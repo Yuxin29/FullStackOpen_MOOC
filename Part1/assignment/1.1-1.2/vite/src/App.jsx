ea61c093a6c2ce7ea194103b853221648a02976f
Mikko <yuxin.29.wu@gmail.com>
2025-11-02 18:24:40 +0200

    vite working

---
+++ Part1/assignment/1.1-1.2/vite/src/App.jsx
@@ -0,0 +1,102 @@
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+
+/* ------------------ex 1.1---------------------  */
+// const Header = ({course}) => {
+//   return <h1>{course}</h1>
+// }
+
+// const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+//   return(
+//     <div>
+//        <p>{part1} {exercises1}</p>
+//        <p>{part2} {exercises2}</p>
+//        <p>{part3} {exercises3}</p>
+//     </div>
+//   )
+// }
+
+// const Total = ({exercises1, exercises2, exercises3}) => {
+//   const total = exercises1 + exercises2 + exercises3
+//   return(
+//        <p>Total number of exercises: {total}</p>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Header course={course}/>
+//       <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+//       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+//     </div>
+//   )
+// }
+/* ------------------ex 1.1---------------------  */
+
+/* ------------------original---------------------  */
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+/* ------------------original---------------------  */
+
+/* ------------------ex 1.2---------------------  */
+const Header = ({course}) => {
+  return <h1>{course}</h1>
+}
+
+const Part = ({partName, exerciseNumber}) => {
+  return (
+    <p>{partName}, {exerciseNumber}</p>
+  )
+}
+
+const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+  return (
+    <div>
+      <Part partName={part1} exerciseNumber={exercises1}/>
+      <Part partName={part2} exerciseNumber={exercises2}/>
+      <Part partName={part3} exerciseNumber={exercises3}/>
+    </div>
+  )
+}
+
+
+const Total = ({exercises1, exercises2, exercises3}) => {
+  const total = exercises1 + exercises2 + exercises3
+  return(
+       <p>Total number of exercises: {total}</p>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={part1} exercises1={exercises1}  part2={part2} exercises2={exercises2}  part3={part3} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
+  )
+}
+/* ------------------ex 1.2---------------------  */
+
+export default App
