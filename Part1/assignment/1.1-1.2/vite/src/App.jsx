606a7df149092542bcd8be6d753ed507fdca84b5
Mikko <yuxin.29.wu@gmail.com>
2025-11-02 22:44:45 +0200

    more js notes and ex

--- Part1/assignment/1.1-1.2/vite/src/App.jsx
+++ Part1/assignment/1.1-1.2/vite/src/App.jsx
@@ -6,6 +6,27 @@ const exercises2 = 7
 const part3 = 'State of a component'
 const exercises3 = 14
 
+
+/* ------------------original---------------------  */
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+/* ------------------original---------------------  */
+
 /* ------------------ex 1.1---------------------  */
 // const Header = ({course}) => {
 //   return <h1>{course}</h1>
@@ -38,26 +59,6 @@ const exercises3 = 14
 //   )
 // }
 /* ------------------ex 1.1---------------------  */
-
-/* ------------------original---------------------  */
-// const App = () => {
-//   return (
-//     <div>
-//       <h1>{course}</h1>
-//       <p>
-//         {part1} {exercises1}
-//       </p>
-//       <p>
-//         {part2} {exercises2}
-//       </p>
-//       <p>
-//         {part3} {exercises3}
-//       </p>
-//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-//     </div>
-//   )
-// }
-/* ------------------original---------------------  */
 
 /* ------------------ex 1.2---------------------  */
 const Header = ({course}) => {
@@ -98,5 +99,83 @@ const App = () => {
   )
 }
 /* ------------------ex 1.2---------------------  */
+
+// /* ------------------ex 1.3---------------------  */
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = {
+//     name: 'Fundamentals of React',
+//     exercises: 10
+//   }
+//   const part2 = {
+//     name: 'Using props to pass data',
+//     exercises: 7
+//   }
+//   const part3 = {
+//     name: 'State of a component',
+//     exercises: 14
+//   }
+
+//   return (
+//     <div>
+//       ...
+//     </div>
+//   )
+// }
+// /* ------------------ex 1.3---------------------  */
+
+// /* ------------------ex 1.4---------------------  */
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const parts = [
+//     {
+//       name: 'Fundamentals of React',
+//       exercises: 10
+//     },
+//     {
+//       name: 'Using props to pass data',
+//       exercises: 7
+//     },
+//     {
+//       name: 'State of a component',
+//       exercises: 14
+//     }
+//   ]
+
+//   return (
+//     <div>
+//       ...
+//     </div>
+//   )
+// }
+// /* ------------------ex 1.4---------------------  */
+
+// /* ------------------ex 1.5---------------------  */
+// const App = () => {
+//   const course = {
+//     name: 'Half Stack application development',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         exercises: 10
+//       },
+//       {
+//         name: 'Using props to pass data',
+//         exercises: 7
+//       },
+//       {
+//         name: 'State of a component',
+//         exercises: 14
+//       }
+//     ]
+//   }
+
+//   return (
+//     <div>
+//       ...
+//     </div>
+//   )
+// }
+// /* ------------------ex 1.5---------------------  */
 
 export default App
