9f79283b3951e19348b611125cb1cd5f8127481f
Yuxin Wu <yuxin.1.wu@gmail.com>
2026-01-12 16:38:42 +0200

    courseinfo prograssing

--- Part2/assignment/phonebook/src/App.jsx
+++ Part2/assignment/phonebook/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 
+// ====================================
+// ex 2.6-2.8
+// ====================================
 const App = () => {
 
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+
   const [newName, setNewName] = useState('')
-  
   const addName = (event) => {
     event.preventDefault()
+    const isThere = persons.some(person => person.name === newName)
+    if (isThere) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
     const nameObject = { name: newName }
     setPersons(persons.concat(nameObject))
     setNewName('')
@@ -20,9 +28,10 @@ const App = () => {
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit"> add </button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => <li key={person.name}>{person.name}</li>)}
