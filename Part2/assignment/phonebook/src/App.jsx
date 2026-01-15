c859244646f2c1e2ae69f9e6086852720fbd6b2f
Yuxin Wu <yuxin.1.wu@gmail.com>
2026-01-15 14:50:18 +0200

    2.14 done

--- Part2/assignment/phonebook/src/App.jsx
+++ Part2/assignment/phonebook/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect, use } from 'react'
-import axios from "axios"
+import { useState, useEffect } from 'react'
+import phonebookService from './services/phonebook'
+import axios from 'axios'
+
 
 // ====================================
 // ex 2.6-2.9
@@ -245,9 +247,114 @@ import axios from "axios"
 
 // ====================================
 // ex 2.12
-// Currently, the numbers that are added to the phonebook are not saved to a backend server. 
+// Currently, 
+// the numbers that are added to the phonebook are not saved to a backend server. 
 // Fix this situation.
 // ====================================
+// const Filter = ({filter, setFilter}) => {
+//   return (
+//     <div> 
+//     filter shown with {' '} 
+//     <input value={filter} onChange={(e) => setFilter(e.target.value)}/>
+//     </div>
+//   )
+// }
+
+// const PersonForm = ({newName, setNewName, newNumber, setNewNumber, addPerson}) => {
+//   return (
+//     <form onSubmit={addPerson}>
+//       <div>
+//         name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+//       </div>
+//       <div>
+//         number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+//       </div>
+//       <div>
+//         <button type="submit"> add </button>
+//       </div>
+//     </form>
+//   )
+// }
+
+// // show all the data in the infor
+// const Persons = ({persons}) => {
+//   return (
+//     <ul>
+//       {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+//     </ul>
+//   )
+// }
+
+// const App = () => {
+//   // getting the backend js data
+//   const [persons, setPersons] = useState([])
+//   useEffect(() => {
+//     axios
+//       .get('http://localhost:3001/persons')
+//       .then(response => {
+//         setPersons(response.data)
+//       })
+//   }, [])
+
+//   const [newName, setNewName] = useState('')
+//   const [newNumber, setNewNumber] = useState('')
+//   const [filter, setFilter] = useState('')
+
+//   // add new person states
+//   const addPerson = (event) => {
+//     event.preventDefault()
+//     const isThere = persons.some(person => person.name === newName)
+//     if (isThere) {
+//       alert(`${newName} is already added to phonebook`)
+//       return
+//     }
+//     const nameObject = {
+//       name: newName, 
+//       number: newNumber 
+//     }
+//     // setPersons(persons.concat(nameObject)) // added to only local, but not updated to the backend                                                                 
+//     // setNewName('')
+//     // setNewNumber('')
+//     axios
+//       .post('http://localhost:3001/persons', nameObject)
+//       .then(response => {
+//         setPersons(persons.concat(response.data)) 
+//         //could concat nameObject as well, but concat response.data is better                                                             
+//         setNewName('')
+//         setNewNumber('')
+//       })
+//   }
+
+//   // filter persons states: only show the name
+//     const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+  
+//   //return jsx
+//   return (
+//     <div>
+//       <h2>Phonebook</h2>
+//       <Filter filter={filter} setFilter = {setFilter}/>
+
+//       <h3>add a new</h3>
+//       <PersonForm
+//         newName={newName}
+//         setNewName={setNewName}
+//         newNumber={newNumber}
+//         setNewNumber={setNewNumber}
+//         addPerson={addPerson}
+//       />
+
+//       <h3>Numbers</h3>
+//       <Persons persons={filteredPersons}/>
+//     </div>
+//   )
+// }
+
+// ====================================
+// ex 2.13
+// Extract the code that handles the communication with the backend into its own module
+// by following the example shown earlier in this part of the course material.
+// ====================================
+
 const Filter = ({filter, setFilter}) => {
   return (
     <div> 
@@ -273,6 +380,7 @@ const PersonForm = ({newName, setNewName, newNumber, setNewNumber, addPerson}) =
   )
 }
 
+// show all the data in the infor
 const Persons = ({persons}) => {
   return (
     <ul>
@@ -282,21 +390,11 @@ const Persons = ({persons}) => {
 }
 
 const App = () => {
-  // default persons data, hard coded
-  // const [persons, setPersons] = useState([
-  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  // ])
+  // getting the backend js data
   const [persons, setPersons] = useState([])
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
-
+    phonebookService.getAll()
+    .then(response => {setPersons(response.data)})
   }, [])
 
   const [newName, setNewName] = useState('')
@@ -311,14 +409,21 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    const nameObject = { name: newName, number: newNumber }
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-    setNewNumber('')
+    const nameObject = {
+      name: newName, 
+      number: newNumber 
+    }
+    phonebookService.create(nameObject) 
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        //could concat nameObject as well, but concat response.data is better                                                             
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
-  // filter persons states
-  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+  // filter persons states: only show the name
+    const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
   
   //return jsx
   return (
@@ -340,11 +445,5 @@ const App = () => {
     </div>
   )
 }
-
-// ====================================
-// ex 2.13
-// Extract the code that handles the communication with the backend into its own module
-//  by following the example shown earlier in this part of the course material.
-// ====================================
 
 export default App
