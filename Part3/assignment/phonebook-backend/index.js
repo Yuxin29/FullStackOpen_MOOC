67f5b82ece5cc2d0b4e855a9b658759be9a1d1e8
Yuxin Wu <yuxin.1.wu@gmail.com>
2026-01-18 17:44:07 +0200

    up to 8

--- Part3/assignment/phonebook-backend/index.js
+++ Part3/assignment/phonebook-backend/index.js
@@ -1,8 +1,19 @@
 
 //import http from 'http';
 import express from 'express';
+import morgan from 'morgan';
+
 const app = express()
+
 app.use(express.json())
+
+app.use(morgan('tiny'))
+
+morgan.token('body', (request) => {
+    return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let phonebook = [
     { 
@@ -28,7 +39,7 @@ let phonebook = [
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello This is my phonebook 3.1 - 3,3 !</h1>')
+  response.send('<h1>Hello This is my phonebook 3.1 - 3,8 !</h1>')
 })
 
 // ex 3.1
@@ -102,27 +113,15 @@ app.post('/api/persons', (request, response) => {
 })
 
 // 3.7: Phonebook backend step 7
-// Add the morgan middleware to your application for logging. 
-// Configure it to log messages to your console based on the tiny configuration.
-// The documentation for Morgan is not the best, 
-// and you may have to spend some time figuring out how to configure it correctly. 
-// However, most documentation in the world falls under the same category, 
-// so it's good to learn to decipher and interpret cryptic documentation in any case.
-// Morgan is installed just like all other libraries with the npm install command. 
-// Taking morgan into use happens the same way as configuring any other middleware by using the app.use command.
+// npm install morgan 
 
 // 3.8*: Phonebook backend step 8
 // Configure morgan so that it also shows the data sent in HTTP POST requests:
-// terminal showing post data being sent
-// Note that logging data even in the console can be dangerous 
-// since it can contain sensitive data and may violate local privacy law (e.g. GDPR in EU) or business-standard. 
-// In this exercise, you don't have to worry about privacy issues, 
-// but in practice, try not to log any sensitive data.
-// This exercise can be quite challenging, even though the solution does not require a lot of code.
-// This exercise can be completed in a few different ways. 
+// terminal showing post data being sent 
 // One of the possible solutions utilizes these two techniques:
-//     creating new tokens
-//     JSON.stringify
+//     creating new tokens  
+//     JSON.stringify         chose this one
+
 
 const PORT = 3001
 app.listen(PORT)
